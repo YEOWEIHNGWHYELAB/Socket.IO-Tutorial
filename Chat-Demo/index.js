6b91abe0432d8884150fa4e35ee19fbfb672af3c
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-01-04 15:37:54 +0800

    ChatApp-Demo Basic client connect and disconnect

--- Chat-Demo/index.js
+++ Chat-Demo/index.js
@@ -1,28 +1,21 @@
-// Express initializes app to be a function handler that you can supply to an HTTP server
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-// We define a route handler / that gets called when we hit our website home
-/*
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
-*/
-
-/*
-So far in index.js we’re calling res.send and passing it a string of HTML. Our code would 
-look very confusing if we just placed our entire application’s HTML there, so instead we're 
-going to create a index.html file and serve that instead.
-
-Let’s refactor our route handler to use sendFile instead
-*/
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// We make the http server listen on port 3000
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
